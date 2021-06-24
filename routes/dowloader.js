94d8607f5b01c661fc709e52100c034ea64c25c9
Veldas R Durai <veldasrdurai72@gmail.com>
2021-06-24 17:01:19 +0530

    scrapped youtube transcript for given link

--- routes/dowloader.js
+++ routes/dowloader.js
@@ -5,21 +5,27 @@ const show = async ( link ) => {
     const page = await browser.newPage();
     await page.goto( link );
     console.log('done');
+
     await page.waitForSelector('div#info.style-scope.ytd-video-primary-info-renderer');
-    await page.evaluate( () => document.querySelectorAll('div#info.style-scope.ytd-video-primary-info-renderer')[0].querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer')[0].click() )
-
-    await page.waitForSelector('ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer');
-    await page.evaluate( () => document.querySelectorAll('ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer')[0].click());
+    await page.evaluate( () => document.querySelectorAll('div#info.style-scope.ytd-video-primary-info-renderer')[0]
+        .querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer')[0].click() )
     
-    await page.waitForSelector('div#header.style-scope.ytd-engagement-panel-title-header-renderer yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer');
-    await page.evaluate( () => document.querySelectorAll('div#header.style-scope.ytd-engagement-panel-title-header-renderer yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer')[0].click() );
+    await page.waitForSelector('tp-yt-iron-dropdown.style-scope.ytd-popup-container');
+    if ( await page.evaluate( () => {
+        const options = document.querySelectorAll('ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer');
+        options.length ?  (() => { options[0].click(); return false; })() : true ;
+    })) return 'No transcript available 1';
+    try{ await page.waitForSelector('ytd-transcript-body-renderer.style-scope.ytd-transcript-renderer');
+    }catch(e){
+        return 'No transcript available 2'
+    }
+    const transcript = await page.evaluate( () => {
+        const allNode = document.querySelectorAll('ytd-transcript-body-renderer.style-scope.ytd-transcript-renderer')[0];
+        // const eachNode = document.querySelectorAll('ytd-transcript-body-renderer.style-scope.ytd-transcript-renderer div.cue-group.style-scope.ytd-transcript-body-renderer');
+        return allNode ?  allNode.innerText : 'No transcript available 2';
+    });
 
-    // const transcript = await page.evaluate( () => {
-    //     // document.querySelectorAll('tp-yt-iron-dropdown.style-scope.ytd-popup-container')[0].querySelectorAll('ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer')[0].click();
-    //     return document.querySelectorAll('ytd-transcript-body-renderer.style-scope.ytd-transcript-renderer')[0].innerText;
-    //     // return 'hello';
-    // });
-    return transcript;
+    return transcript.split(/\n{0,1}\d{2}:\d{2}\n/g).join(' ') ;
     // await browser.close();
 };
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const show = require('./dowloader');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const transcript = await show('https://www.youtube.com/watch?v=2VEwphv-ro4');
+  const transcript = await show('https://www.youtube.com/watch?v=5aLkdkoiahk&t=3s');
   res.render('index', { title: 'Express', transcript });
 });
 
