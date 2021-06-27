52b09e4169098d2ec7abc809958f6ff7cd9bcf66
Veldas R Durai <veldasrdurai72@gmail.com>
2021-06-27 21:09:04 +0530

    conpleted the designing of the first part of the page

--- public/javascripts/trails/scrolling.js
+++ public/javascripts/trails/scrolling.js
@@ -3,8 +3,16 @@ document.addEventListener( 'scroll', () => {
     // var randomNum = Math.floor((Math.random() * 4) + 1);
     // console.log( randomNum );
 
-    // const sub = document.querySelector('#header #header-text #sub');
-    // const head = document.querySelector('#header #header-text #head');
+    if( window.scrollY >= 250 && window.scrollY <= 550 ) {
+        document.querySelector('#free-space #move').style.right = ( screen.width * (550 - window.scrollY) /300 ) +'px';
+    } else if( window.scrollY < 250){
+        document.querySelector('#free-space #move').style.right = (screen.width)+'px';
+    } else {
+        document.querySelector('#free-space #move').style.right = '0px';
+    }
+
+    const sub = document.querySelector('#header #header-text #sub');
+    const head = document.querySelector('#header #header-text #head');
     // if(randomNum === 4){
     //     sub.style.border = 'none';
     //     head.style.border = 'none';
@@ -28,30 +36,31 @@ document.addEventListener( 'scroll', () => {
     // }
 
     
-    // sub.style.border = 'none';
-    // head.style.border = 'none';
+    sub.style.border = 'none';
+    head.style.border = 'none';
+    
+    var colors = ['red','green','blue','white'];
+    var randomNum1 = Math.floor((Math.random() * 4) + 1);
 
-    // var randomNum1 = Math.floor((Math.random() * 4) + 1);
+    if( randomNum1 === 4 ){
+        sub.style.borderLeft = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    } else if ( randomNum1 === 3 ){
+        sub.style.borderRight = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)]; 
+    } else if( randomNum1 === 2 ){
+        sub.style.borderTop = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    } else {
+        sub.style.borderBottom = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    }
 
-    // if( randomNum1 === 4 ){
-    //     sub.style.borderLeft = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // } else if ( randomNum1 === 3 ){
-    //     sub.style.borderRight = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16); 
-    // } else if( randomNum1 === 2 ){
-    //     sub.style.borderTop = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // } else {
-    //     sub.style.borderBottom = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // }
+    var randomNum2 = Math.floor((Math.random() * 4) + 1);
 
-    // var randomNum2 = Math.floor((Math.random() * 4) + 1);
-
-    // if( randomNum2 === 4 ){
-    //     head.style.borderLeft = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // } else if ( randomNum2 === 3 ){
-    //     head.style.borderRight = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16); 
-    // } else if( randomNum2 === 2 ){
-    //     head.style.borderTop = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // } else {
-    //     head.style.borderBottom = Math.floor((Math.random() * 2) + 1) + 'px solid #' + Math.floor(Math.random()*16777215).toString(16);
-    // }
+    if( randomNum2 === 4 ){
+        head.style.borderLeft = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    } else if ( randomNum2 === 3 ){
+        head.style.borderRight = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)]; 
+    } else if( randomNum2 === 2 ){
+        head.style.borderTop = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    } else {
+        head.style.borderBottom = Math.floor((Math.random() * 4) + 1) + 'px solid ' + colors[Math.floor(Math.random() * 4)];
+    }
 });
